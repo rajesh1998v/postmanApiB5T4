9ec0c38d04e3785fd9ec3a4d17444d4a212f3f50
Rajesh kumar <rakumarv87@gmail.com>
2023-06-23 14:08:14 +0530

    postAPi

---
+++ postmanApi.js
@@ -0,0 +1,86 @@
+var express = require("express");
+
+var app = express();
+app.use(express.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept,Authorization"
+  );  
+  res.header("Access-Control-Expose-Headers","Authorization")
+  res.header("Access-Control-Expose-Headers","X-Auth-Token")
+  res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
+  next();
+});
+
+let port =2420;
+
+let axios = require("axios");
+app.listen(port, () => console.log(`Node app listening on port ${port}`));
+
+app.post("/post",async function(req,res){
+  let body = req.body;
+  let token = body.headerKey1.toLowerCase()=="authorization"?body.headerValue1:
+              body.headerKey2.toLowerCase()=="authorization"?body.headerValue2:
+              body.headerKey3.toLowerCase()=="authorization"?body.headerValue3:"";
+    if(body.req==="GET"){
+        try{
+          let response = await axios.get(body.url,{headers: {authorization: token}});
+          let resData = JSON.stringify(response.data);
+          res.status(200).send(resData);
+          
+        }catch(error){
+          if (error.response){
+            let { status, statusText } = error.response;
+            res.status(status).send(error.response);
+          }else res.status(404).send(error);
+        }
+     
+
+    }else if(body.req==="POST"){
+      try{
+        let body1 = JSON.parse(body.body);
+        console.log(body1);
+        let response = await axios.post(body.url,body1, {headers: {authorization: token}}) ;
+        let resData = JSON.stringify(response.data);
+          res.status(200).send(resData);
+    }catch (error){
+        if (error.response){
+            let { status, statusText } = error.response;
+            res.status(status).send(error.response);
+            console.log(error.response);
+        }else res.status(404).send(error);
+    }
+       
+    
+    }else if(body.req==="PUT"){
+
+      try{
+        let body1 = JSON.parse(body.body);
+        let response = await axios.put(body.url,body1, {headers: {authorization: token}}) ;
+        let resData = JSON.stringify(response.data);
+          res.status(200).send(resData);
+        console.log(response.data);
+      }catch (error){
+        if (error.response){
+            let { status, statusText } = error.response;
+            res.status(status).send(error.response);
+        }else res.status(404).send(error);
+      } 
+       
+    }else if(body.req==="DELETE"){
+      try{
+        let response = await axios.delete(body.url, {headers: {authorization: token}}) ;
+        let resData = JSON.stringify(response.data);
+          res.status(200).send(resData);
+        console.log(response.data);
+      }catch (error){
+        if (error.response){
+            let { status, statusText } = error.response;
+            res.status(status).send(error.response);
+        }else res.status(404).send(error);
+      } 
+    }
+
+})
